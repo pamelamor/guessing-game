979998ae7fb16fc395855cb2daa487d8105a80d3
portiz491 <ortizp@dickinson.edu>
2023-02-28 12:48:17 -0500

    Use a function for the game logic

--- game.py
+++ game.py
@@ -5,26 +5,30 @@ import random
 print("Howdy, what's your name?")
 # Get user name
 userName = input("(type in your name) ")
+# Choose a random number
+randomNumber = random.randint(0, 100)
 # Prompt the user to pick a number between 1 and 100
 print(userName + ", I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
 # Get user guess
-userGuess = int(input("Your guess? "))
-# Choose a random number
-randomNumber = random.randint(0, 100)
-tries = 0
+# userGuess = int(input("Your guess? "))
 
-while userGuess != randomNumber: 
-    
-    if userGuess > randomNumber:
-        tries += 1
-        print("Your guess is too high, try again.")
-    elif userGuess < randomNumber:
-        tries += 1
-        print("Your guess is too low, try again.")
+
+def play_game():
     userGuess = int(input("Your guess? "))
+    tries = 0
+    while userGuess != randomNumber: 
+        print(userGuess)
+        print(randomNumber)
+        if userGuess > randomNumber:
+            tries += 1
+            print("Your guess is too high, try again.")
+        elif userGuess < randomNumber:
+            tries += 1
+            print("Your guess is too low, try again.")
+        userGuess = int(input("Your guess? "))
         
-if userGuess == randomNumber: 
-    print("Well done, " + userName + "! You found my number in " + str(tries) + " tries!")
+    if userGuess == randomNumber: 
+        print("Well done, " + userName + "! You found my number in " + str(tries) + " tries!")
 
-
+play_game()
