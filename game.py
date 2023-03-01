49e78a2a58085c8f4b440c000c3dee35bcc84923
portiz491 <ortizp@dickinson.edu>
2023-02-28 21:45:03 -0500

    3/5 Further Study solutions

--- game.py
+++ game.py
@@ -6,14 +6,18 @@ def play_game():
     print("Howdy, what's your name?")
     # Get user name
     userName = input("(type in your name) ")
-    # Choose a random number
-    randomNumber = random.randint(0, 100)
-    # Prompt the user to pick a number between 1 and 100
-    print(userName + ", I'm thinking of a number between 1 and 100.")
-    print("Try to guess my number.")
+    # Allow user to chose the numerical range of the game
+    lowerBound, higherBound = input("Enter a numerical range to play for: "). split()
+    randomNumber = random.randint(int(lowerBound), int(higherBound))
+    # Prompt the user to make a guess
+    print(userName + ", I'm thinking of a number between " + lowerBound  + " and "  + higherBound  + ". Try to guess my number.")
     userGuess = int(input("Your guess? "))
     tries = 0
     while userGuess != randomNumber: 
+        if(tries == 20):
+            anotherRound = input("Too many tries! Would you like to restart another round (Type 'Yes' or 'No')? ")
+            if(anotherRound == "Yes"):
+                play_game()
         if userGuess > randomNumber:
             tries += 1
             print("Your guess is too high, try again.")
@@ -21,8 +25,14 @@ def play_game():
             tries += 1
             print("Your guess is too low, try again.")
         userGuess = int(input("Your guess? "))
-        
+
     if userGuess == randomNumber: 
         print("Well done, " + userName + "! You found my number in " + str(tries) + " tries!")
-
+        
+        anotherRound = input("Would you like to pay another round (Type 'Yes' or 'No')? ")
+        if(anotherRound == "Yes"):
+            play_game()
+        else:
+            return
+        
 play_game()
